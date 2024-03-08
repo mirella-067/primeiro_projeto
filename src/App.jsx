7f1071bb23d40b78e102fb093e3c1044ca8a2fea
maousama1432 <131804292+maousama1432@users.noreply.github.com>
2024-03-07 21:47:15 -0400

    atualização
    mesmo atrasada está aqui a atividade

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import Header from "./Components/Header";
+import Footer from './Components/Footer';
+
+import "./globals.css";
+
 export default function App() {
 
-  const nome = "João";
-  const sobreNome = "Santos";
-
   return(
-    <div>
-        <h1>Hello World</h1>
-        <p>Desenvolvido por: {nome}</p>
-    </div>  
+    <main>
+      <Header/>
+      <Footer/>
+    </main>
   );
 
 
